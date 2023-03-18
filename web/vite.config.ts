595f06eabc05b274b061dbb25c734ed754230981
Jacob Bogers <jkfbogers@gmail.com>
2023-03-18 17:23:13 +0100

    using published rollup/vite plugin to remove jsx data-testid attribute

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
-import removeTestIdAttribute from './tools/vite-jsx-remove-attributes';
+import removeTestIdAttribute from 'rollup-plugin-jsx-remove-attributes';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: { sourcemap: true },
     plugins: [
         react(),
-        removeTestIdAttribute({ attributes: ['data-testid'] }),
+        removeTestIdAttribute({
+            //include:['**/**/App.tsx'],
+            attributes: ['data-testid'],
+            usage: 'vite'
+        }),
         eslint(),
         svgr({
             svgrOptions: {
